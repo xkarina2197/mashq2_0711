490f450cf97cc0cb497bbbd89cd9ad202799ffe0
xkarina2197 <xkarina2197@gmail.com>
2025-11-07 18:05:24 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+# 1-misol
+for i in range(1, 31):
+    if i % 2 == 0:
+        print(i)
+
+# 2-misol
+sum = 0
+for i in range(1, 51):
+    sum += i
+    if i % 5 == 0:
+        continue
+    print(sum)
+
+# 3-misol
+for  i in range(-10,11):
+    if i < 0:
+        continue
+    print(i * i)
+
+# 4-misol
+sum = 0
+for i in range(1, 100):
+     sum += i
+     if i % 3 != 0:
+         continue
+print(sum)
